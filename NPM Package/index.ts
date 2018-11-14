b0c9f488c776f8383eb4f6551ae32860c35b59b6
Steffenbp <steff.bpoulsen@gmail.com>
2018-11-14 09:34:07 +0100

    Trying to strinify Error object

--- NPM Package/index.ts
+++ NPM Package/index.ts
@@ -1,15 +1,22 @@
-
+import { writeFile } from 'fs'
 
 class App {
 	public subscribe(){
 
 	}
-
-	
-	public run(){
-
-	}
-
+}
+try 
+{
+	throw new Error("test error string");
+}
+catch (e) {
+	var err:string = JSON.stringify(e, Object.keys(e));
 }
 
-throw new Error();
+writeFile(`${__dirname}/test.txt`, err, (error) => {
+	if (error){
+		console.error(error.stack);
+		return;
+	}
+	console.log("File has been created");
+});
