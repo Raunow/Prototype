71c8be53a5ef8aacfe29a5e1b2f326f64f00b95e
Steffenbp <steff.bpoulsen@gmail.com>
2018-11-14 12:17:54 +0100

    W.I.P. Get JSON from Stacktrace

--- NPM Package/index.ts
+++ NPM Package/index.ts
@@ -5,15 +5,11 @@ class App {
 
 	}
 }
-try 
-{
-	throw new Error("test error string");
-}
-catch (e) {
-	var err:string = JSON.stringify(e, Object.keys(e));
-}
+try { throw new Error("test error string"); }
+catch (e) { var err:string = JSON.stringify(e, Object.keys(e)); }
 
-writeFile(`${__dirname}/test.txt`, err, (error) => {
+
+writeFile(`${__dirname}/test.json`, err, (error) => {
 	if (error){
 		console.error(error.stack);
 		return;
