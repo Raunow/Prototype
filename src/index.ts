fba4f4e80858671ec491758999ab1ecba9acd163
Steffen P <steff.bpoulsen@gmail.com>
2019-11-15 13:12:15 +0100

    removed testcode

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
 import { createExpress } from './express/app';
 import { WorkerPool } from './workers/WorkerPool';
 
-export const workerPool = new WorkerPool(`${__dirname}\\workers\\worker.js`, 4);
+export const workerPool = new WorkerPool(`${__dirname}\\workers\\worker.js`, 8);
 
 async function main() {
-	//startWorker();
-
 	let app = createExpress(PORT);
 
 	app.then(() => console.log(`Server listening on http://localhost:${PORT}/task/`));
