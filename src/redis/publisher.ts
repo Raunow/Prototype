f8f0117ef95ac7d2901707cc7167444643c486fd
Steffen P <steff.bpoulsen@gmail.com>
2019-11-15 13:14:23 +0100

    Maybe useful later

--- src/redis/publisher.ts
+++ src/redis/publisher.ts
@@ -1,32 +1,32 @@
-import Redis from 'ioredis';
+// import Redis from 'ioredis';
 
-const redis = new Redis({ port: 6379, host: '127.0.0.1' });
-let keyPrefix = 'dev-';
+// const redis = new Redis({ port: 6379, host: '127.0.0.1' });
+// let keyPrefix = 'dev-';
 
-async function main() {
-	let key = 'version';
-	let val = await redis.get(key);
+// async function main() {
+// 	let key = 'version';
+// 	let val = await redis.get(key);
 
-	if (!val) {
-		await redis.multi()
-			.set(keyPrefix + key, '1.0.10')
-			.publish(keyPrefix + key, '1.0.10')
-			.exec();
-	}
+// 	if (!val) {
+// 		await redis.multi()
+// 			.set(keyPrefix + key, '1.0.10')
+// 			.publish(keyPrefix + key, '1.0.10')
+// 			.exec();
+// 	}
 
-	console.log('Publisher started');
-	let topic = 'TEST';
+// 	console.log('Publisher started');
+// 	let topic = 'TEST';
 
-	process.stdout.write(topic + ': ');
-	process.openStdin().addListener('data', data => {
-		let input: string = data.toString().trim();
-		if (input.startsWith('>')) {
-			topic = input.slice(1);
-		} else {
-			redis.publish(topic, input);
-		}
-		process.stdout.write(topic + ': ');
-	});
-}
+// 	process.stdout.write(topic + ': ');
+// 	process.openStdin().addListener('data', data => {
+// 		let input: string = data.toString().trim();
+// 		if (input.startsWith('>')) {
+// 			topic = input.slice(1);
+// 		} else {
+// 			redis.publish(topic, input);
+// 		}
+// 		process.stdout.write(topic + ': ');
+// 	});
+// }
 
-main();
+// main();
--- src/redis/subscriber.ts
+++ src/redis/subscriber.ts
@@ -1,26 +1,26 @@
-import Redis from 'ioredis';
+// import Redis from 'ioredis';
 
-const redis = new Redis({ port: 6379, host: '127.0.0.1', autoResubscribe: true });
+// const redis = new Redis({ port: 6379, host: '127.0.0.1', autoResubscribe: true });
 
-console.log('Starting')
+// console.log('Starting')
 
-redis.on('pmessage', (pattern, channel, message) => {
-	console.log('%s %s: %s', pattern, channel, message);
-})
+// redis.on('pmessage', (pattern, channel, message) => {
+// 	console.log('%s %s: %s', pattern, channel, message);
+// })
 
-redis.on('message', (channel, message) => {
-	console.log('%s: %s', channel, message);
-})
+// redis.on('message', (channel, message) => {
+// 	console.log('%s: %s', channel, message);
+// })
 
-redis.psubscribe('*');
-redis.psubscribe('test/*');
+// redis.psubscribe('*');
+// redis.psubscribe('test/*');
 
-redis.subscribe('dev-version');
-redis.subscribe('TEST', (error, count) => {
-	if (error) {
-		throw new Error(error);
-	}
-	console.log(`Subbed to ${count} channels`);
-});
+// redis.subscribe('dev-version');
+// redis.subscribe('TEST', (error, count) => {
+// 	if (error) {
+// 		throw new Error(error);
+// 	}
+// 	console.log(`Subbed to ${count} channels`);
+// });
 
 
