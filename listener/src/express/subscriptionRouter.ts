cc6f21e922acee14480edc8c2b3162b232e77b33
Steffen P <steff.bpoulsen@gmail.com>
2019-11-19 15:29:51 +0100

    Initial express structure

--- listener/src/express/subscriptionRouter.ts
+++ listener/src/express/subscriptionRouter.ts
@@ -1,72 +1,31 @@
 import { json } from 'body-parser';
 import { Request, Response, Router } from 'express';
-import { readFile, unlink, writeFile } from 'fs';
 import { join } from 'path';
-import { RespondHTTP } from './response';
 
 class SubscriptionController {
-	private resolvePath = (filename: string) => join(__dirname, '../..', `/accounts/${filename}.json`);
+	private resolvePath = (account: string) => join(__dirname, '../..', `/accounts/${account}.json`);
 
 	PUT({ params, body }: Request, res: Response) {
-		writeFile(this.resolvePath(params.name), JSON.stringify({
-			name: params.name,
-			task: body.task,
-			imports: body.imports || []
-		}), (err) => {
-			if (err) {
-				console.log(err);
-				RespondHTTP(res, 500, err);
-			} else {
-				RespondHTTP(res, 200, params.name + ' created');
-			}
-		});
+
 	}
 
 	GET({ params }: Request, res: Response) {
-		readFile(this.resolvePath(params.name), (err, data) => {
-			if (err) {
-				console.log(err);
-				RespondHTTP(res, 500, err);
-			} else {
-				RespondHTTP(res, 200, JSON.parse(data.toString()));
-			}
-		});
+
 	}
 
-	POST({ params, body }: Request, res: Response) {
-		workerPool.run(() => ({
-			filename: params.name,
-			context: body.context || {}
-		}),
-			(err, result) => {
-				if (err) {
-					console.log(err);
-					RespondHTTP(res, 500, 'Error: Task not present.');
-				} else {
-					if (result.error) {
-						console.log(result)
-						result.error = `${result.error.name}: ${result.error.message}`
-					}
-					RespondHTTP(res, 200, result);
-				}
-			});
+	POST(req: Request, res: Response) {
+
 	}
+
 	DELETE({ params }: Request, res: Response) {
-		unlink(this.resolvePath(params.name), (err) => {
-			if (err) {
-				console.log(err);
-				RespondHTTP(res, 500, 'Error: Task not present.');
-			} else {
-				RespondHTTP(res, 200, params.name + ' deleted');
-			}
-		});
+
 	}
 }
 
 
 export function subscriptionRouter(...middlewares: Router[]) {
 	let param = '/:account';
-	let path = '/subscription';
+	let path = '/subscription' + param;
 	let subscriber = new SubscriptionController;
 	let router = Router();
 
--- listener/src/index.ts
+++ listener/src/index.ts
@@ -1,1 +1,10 @@
-console.log(`API ready`);
+import { createExpress } from './express/app';
+
+async function main() {
+	let app = createExpress(PORT);
+
+	app.then(() => console.log(`Server listening on http://localhost:${PORT}/subscription/`));
+}
+
+let PORT = 4000;
+main();
