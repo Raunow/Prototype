3394d4141f84f7891e13f1191478715045f161d3
Steffen P <steff.bpoulsen@gmail.com>
2019-12-05 11:57:30 +0100

    More work on reader

--- listener/src/redis/accountReader.ts
+++ listener/src/redis/accountReader.ts
@@ -1,37 +1,25 @@
 import { readFile } from "fs";
 import { join } from "path";
+import { IAccount, IApplication, IBlock, IError, IInput } from "./interfaces";
 
 const resolvePath = (file: string, folder: string) => join(__dirname, '../..', `/config/${folder}/${file}.json`);
 const templatePath = (file: string) => resolvePath(file, 'templates');
 const accountPath = (file: string) => resolvePath(file, 'accounts');
 
-interface IAccount {
-	topic: string;
-	apps: { [key: string]: IApplication };
-}
-interface IApplication {
-	name: string;
-	user: string;
-	args?: { [key: string]: any };
-	error: "all" | "silent" | Array<string>;
-}
-interface IBlock {
-	name: string;
-	title: string;
-	inputs: { [key: string]: any };
-	children: { [key: string]: Block };
-}
 
 
+//TODO: use args before parsing to objects. and pass the args down the chain.
 export class Account implements IAccount {
 	topic: string;
+	args: { [key: string]: any }
 	apps: { [key: string]: Application };
 
 	constructor(public name: string) {
 		readFile(accountPath(this.name), (err, data) => {
 			if (err) console.error(err);
 
-			let { topic, apps } = JSON.parse(data.toString()) as IAccount;
+			let { topic, args, apps } = JSON.parse(data.toString()) as IAccount;
+			this.args = args;
 			this.topic = topic;
 
 			Object.entries(apps).forEach(([name, app]) => this.apps[name] = new Application(app));
@@ -39,29 +27,29 @@ export class Account implements IAccount {
 	}
 }
 
+//TODO: Merge args and check for macros in text before parsing.
 export class Application implements IApplication {
 	name: string;
 	user: string;
 	args?: { [key: string]: any };
-	error: "all" | "silent" | Array<string>;
-	blocks: Array<Block>;
-	triggers: Array<Block>;
+	inputs?: { [key: string]: IInput }
+	error?: IError;
+	block: Block;
 
 	constructor(app: IApplication) {
 		Object.assign(this, app)
 
-		this.initBlocks(this.name);
-	}
-
-	initBlocks(name: string) {
-		let block = new Block(name);
-
-
+		this.block = new Block(this.name);
 	}
 }
 
-export class Block {
-	children: Array<Block>;
+export class Block implements IBlock {
+	name: string;
+	inputs?: { [key: string]: IInput };
+	children?: { [key: string]: IBlock };
+	output?: Array<string>;
+	error: IError;
+
 	constructor(name: string) {
 		readFile(templatePath(name), (err, data) => {
 			if (err) console.error(err);
---
+++ listener/src/redis/interfaces.ts
@@ -0,0 +1,28 @@
+export interface IAccount {
+	topic: string;
+	args?: { [key: string]: any };
+	apps?: { [key: string]: IApplication };
+}
+export interface IApplication {
+	name: string;
+	user: string;
+	args?: { [key: string]: any };
+	inputs?: { [key: string]: IInput }
+	error?: IError;
+}
+export interface IBlock {
+	name: string;
+	inputs?: { [key: string]: IInput };
+	children?: { [key: string]: IBlock };
+	output?: Array<string>;
+	error?: IError;
+}
+export interface IInput {
+	topics?: Array<string>;
+	title?: string;
+	value?: any;
+	writeable?: boolean;
+	connectable?: boolean;
+}
+
+export type IError = "all" | "silent" | Array<string>;
